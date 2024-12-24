2a9d804b7f463cbcf859ddfe0f6f9019f869316c
paolotax <paolo.tassinari@gmail.com>
2024-12-24 14:35:15 +0100

    Add password editing functionality with controller, views, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   root "pages#home"
   resource :dashboard, only: :show
 
   resource :registration, only: %i[new create]
   resource :session, only: %i[new create destroy]
   resource :password_reset, only: %i[new create edit update]
+  resource :password, only: %i[edit update]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
---
+++ test/controllers/passwords_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class PasswordsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:alex)
+  end
+
+  test "#edit" do
+    get edit_password_path
+    assert_response :success
+  end
+
+  test "#update with valid params updates the user's password" do
+    patch password_path, params: {
+      password: {
+        password: "newpassword",
+        password_confirmation: "newpassword",
+        password_challenge: "password"
+      }
+    }
+    assert_redirected_to dashboard_path
+    assert users(:alex).reload.authenticate("newpassword")
+  end
+  
+  test "#update with nil password challenge does not update the user's password" do
+    patch password_path, params: {
+      password: { password: "newpassword", password_confirmation: "newpassword" }
+    }
+    assert_response :unprocessable_entity
+    assert_not users(:alex).reload.authenticate("newpassword")
+  end
+end
