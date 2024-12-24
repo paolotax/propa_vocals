51041267424f603550c79336cc80af9b16f53bc0
paolotax <paolo.tassinari@gmail.com>
2024-12-24 11:25:24 +0100

    Add user model with validations and bcrypt integration

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  MINIMUM_PASSWORD_LENGTH = 8
+
+  has_secure_password
+
+  validates :password, length: { minimum: MINIMUM_PASSWORD_LENGTH }
+  validates :email, presence: true, uniqueness: true
+
+  normalizes :email, with: ->(email) { email.strip.downcase }
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "password must be at least 8 characters" do
+    invalid_password = "a" * (User::MINIMUM_PASSWORD_LENGTH - 1)
+    user = User.new email: "test@email.com", password: invalid_password
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "Password is too short (minimum is 8 characters)"
+  end
+
+  test "password must be present" do
+    user = User.new email: "test@email.com", password: ""
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "Password can't be blank"
+  end
+
+  test "email uniqueness" do
+    user = users(:paolotax).dup
+    user.email = "   #{user.email.upcase}   "
+    user.password = "password"
+
+    assert_not user.valid?
+    assert_equal ["has already been taken"], user.errors[:email]
+  end
+end
